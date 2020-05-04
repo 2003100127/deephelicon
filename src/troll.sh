8dcb5d56e077c26217e66c8926a9aaf089b3d3f7
Jianfeng Sun <jianfeng.sunmt@gmail.com>
2020-05-04 20:28:18 +0200

    release
    release

--- src/troll.sh
+++ src/troll.sh
@@ -7,8 +7,8 @@ GDCA='PATH/TO/GDCA'
 HHBLITS='PATH/TO/HHBLITS'
 
 # SET PATH OF DIRECTORIES
-DB='PATH/TO/HHBLITS/DB/'
-TMHMM='PATH/TO/TMHMM/ROOT/'
+DB='PATH/TO/HHBLITS/DB'
+TMHMM='PATH/TO/TMHMM/ROOT_DIRECTORY/'
 
 while getopts ":n:c:i:" opt
 do
@@ -28,10 +28,11 @@ do
     esac
 done
 
-cat $in$ps$pc'.fasta' | $TMHMM'/bin/decodeanhmm.Linux_x86_64' -f $TMHMM'/lib/TMHMM2.0.options' -modelfile $TMHMM'/lib/TMHMM2.0.model' -noPrintSeq -noPrintScores -noPrintID -noPrintStat > $in$ps$pc'.tmhmm'
 $HHBLITS -i $in$ps$pc'.fasta' -maxfilt 100000 -realign_max 100000 -d $DB -all -B 100000 -Z 100000 -n 3 -e 0.001 -oa3m $in$ps$pc'.a3m'
-egrep -v "^>" $in$ps$pc'.a3m' | sed 's/[a-zA-Z]//g' > $in$ps$pc'.aln'
-egrep -v "^>" $in$ps$pc'.a3m' | sed 's/[a-z]//g' > $in$ps$pc'.faln'
+egrep -v "^>" $in$ps$pc'.a3m' | sed 's/[a-z]//g' > $in$ps$pc'.aln'
+sed 's/[a-z]//g' $in$ps$pc'.a3m' > $in$ps$pc'.faln'
 $CCMPRED $in$ps$pc'.aln' $in$ps$pc'.ccmpred'
-$FREECONTACT < $in$ps$pc'.aln' > $in$ps$pc'.evfold'
-$PLMDCA -o $in$ps$pc'.params' -c $in$ps$pc'.plmc' -n 4 $in$ps$pc'.faln'
+$FREECONTACT -f $in$ps$pc'.aln' > $in$ps$pc'.evfold'
+$PLMDCA -o $in$ps$pc'.params' -c $in$ps$pc'.plmc' -le 16.0 -lh 0.01 -m 200 $in$ps$pc'.faln'
+$GDCA $in$ps$pc'.faln'
+cat $in$ps$pc'.fasta' | $TMHMM'/bin/decodeanhmm.Linux_x86_64' -f $TMHMM'/lib/TMHMM2.0.options' -modelfile $TMHMM'/lib/TMHMM2.0.model' -noPrintSeq -noPrintScores -noPrintID -noPrintStat > $in$ps$pc'.tmhmm'
